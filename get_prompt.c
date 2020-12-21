94e842f50c03f1d3e022c20872cffb11b60e61bb
Gabriel Abdul <gabrielabdul1@gmail.com>
2020-12-21 15:13:21 -0600

    v1: returns rudimentary prompt

---
+++ get_prompt.c
@@ -0,0 +1,12 @@
+#include "shell.h"
+
+/**
+ * get_prompt - returns the prompt, a formatted string
+ * Return: Prompt
+ */
+char *get_prompt(void)
+{
+    char *prompt = "$ ";
+
+    return (prompt);
+}
