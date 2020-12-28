99ecaada8325abe4197b13aec679fad1405eadd2
J.I. Cruz <acounts@jicruz.com>
2020-12-28 03:30:54 -0400

    refactor execute_file.c

--- execute_file.c
+++ execute_file.c
@@ -4,63 +4,43 @@
  * execute_file - executes a file line by line
  * @fd: file descriptor
  * Return: exit status
- **/ 
+ **/
 int execute_file(int fd)
 {
-	/* We can deal with the 'empty prompt' issue much more cleanly this way */
-	char *prompt = get_prompt(fd);
-	char **history;
+	char *prompt = get_prompt(fd), **history = NULL;
 	FILE *fp = fdopen(fd, 'r');
 	exec_f executor = NULL;
-	int loop = 1;
+	int line_no = 0;
 
 	if (isatty(fd))
-	{
-		sprintf(history_path, "%s/%s", homedir, ".hsh_history");
-		free(homedir);
-		history_fd = open(history_path, O_CREAT | O_RDWR | O_APPEND);
-		history_line_no = get_history(history);
-	}
-
-	/* we may be better off using our own getline, come to think of it */
+		history_fd = get_history(history);
 	while (1)
 	{
-		/*
-		while ((line = getline()) != -1):
-			args = tokenize(line);
-			commands = make_commands(args)
-			for command in commands:
-				status = execute(command)
-				save_history()
-		*/
-		/* this is just the expanded-out version of what we wrote in the
-		commented-out section above, with some extra additions */
 		printf("%s", prompt);
 		signal(SIGINT, sigint_handler);
-		if (getline(&line, &line_size, fp) == -1)
-		{
-			if (isatty(fd))
-				save_history_to_file(history);
-			return (status);
-		}
+		if (get_tokens(fd) == NULL)
+			goto CLEANUP;
 
 		args = tokenize(line);
 		command = make_commands(args);
 		for (tmp = command; tmp; tmp = tmp->next)
 		{
-			if (strcmp(tmp->command, "history") == 0)
-				tmp->args = history;
-			/* I made a typedef for a function pointer: typedef int exec_f(command_t *command);
-			That typedef means pointer to a function of type "exec_f", defined as a function that takes
-			a command struct ptr as an input and returns an integer (exit status) as an output
-			get_executor() can use the command's logic to choose an executor (builtin function, wrapped execve,
-			piped execs, etc) and return a pointer to that function */
 			executor = get_executor(command);
+			tmp->line_no = line_no;
+			if (_strcmp(tmp->command, "history") == 0)
+				tmp->args = history;
 			status = executor(tmp);
 			if (isatty(fd))
 				save_line_to_history(line, history);
-			/* Logic about operators and exit statuses goes here probably */
+			if (_strcmp(tmp->command, "exit") == 0)
+				goto CLEANUP;
+			line_no++;
 		}
 		free_command_chain(command);
 	}
+
+CLEANUP:
+	if (isatty(fd))
+		save_history_to_file(history, history_fd);
+	return (status);
 }
