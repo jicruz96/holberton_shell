7e26573752c13ba9f7885dd20ae8d6b541f61bdc
J.I. Cruz <acounts@jicruz.com>
2020-12-28 03:06:09 -0400

    Push custom strings library

---
+++ strings/_strlen.c
@@ -0,0 +1,17 @@
+#include "my_strings.h"
+
+/**
+ * _strlen - custom strlen
+ * @str: string
+ * Return: length
+ **/
+int _strlen(char *str)
+{
+	int i = 0;
+
+	if (str)
+		while (*(str++))
+			i++;
+
+	return (i);
+}
