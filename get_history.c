ca58782df9bb00b5185093e2fdb759c3b08eeff6
J.I. Cruz <acounts@jicruz.com>
2020-12-30 18:41:13 -0400

    Refactor history helper functions

--- get_history.c
+++ get_history.c
@@ -5,32 +5,32 @@
  * @history: array where history will be saved
  * Return: history file descriptor
  **/
-int get_history(char *history[])
+int get_history(char *history[], int *history_size)
 {
-	char path[256];
-	char *line = NULL;
-	char *homedir = _getenv("HOME");
-	size_t line_size = 0;
+	char path[256], *line = NULL, *homedir = _getenv("HOME");
 	int i = 0, fd;
-	FILE *fp;
 
 	sprintf(path, "%s/%s", homedir, ".hsh_history");
+	free(homedir);
+
 	fd = open(path, O_CREAT | O_RDWR | O_APPEND, 0644);
 	if (fd == -1)
 	{
 		perror("hsh");
 		return (-1);
 	}
-	fp = fdopen(fd, "r+");
-	while (getline(&line, &line_size, fp) != -1)
+	
+	for (i = 0; (line = _getline(fd)) != -1; i++)
 	{
 		if (i == HISTSIZE)
-			for (; i; i--)
-				history[i] = NULL;
+			while (i)
+				free(history[i]), history[i--] = NULL;
 		history[i] = line;
-		line = NULL;
-		line_size = 0;
-		i += 1;
 	}
+
+	*history_size = i;
+	if (isatty(STDIN_FILENO))
+		shell.lines = i;
+	
 	return (fd);
 }
