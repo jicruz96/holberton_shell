789eb991b2df9e0a9479190b5e746af7613bd485
Gabriel Abdul <gabrielabdul1@gmail.com>
2021-01-11 22:47:34 -0600

    refactor shell.c

--- shell.c
+++ shell.c
@@ -1,5 +1,4 @@
 #include "shell.h"
-#define CANT_OPEN 127
 
 shell_t shell = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
@@ -11,44 +10,35 @@ shell_t shell = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
  **/
 int main(int argc, char *argv[])
 {
-	alias_t *tmp;
-	int fd = STDIN_FILENO, i;
-	char error_msg[256];
+	int fd = STDIN_FILENO;
+	char *shellname = argv[0], error_msg[256];
 
-	/* If an argument was passed, execute that and exit */
+	/* If an argument was passed, get its file descriptor */
 	if (argc > 1)
 	{
 		fd = open(argv[1], O_RDONLY);
+		/* if file does not exist, print error and exit */
 		if (fd == -1)
 		{
 			sprintf(error_msg, "%s: 0: Can't open %s\n", argv[0], argv[1]);
 			write(STDERR_FILENO, error_msg, _strlen(error_msg));
 			return (CANT_OPEN);
 		}
-		shell_init(argv[1], fd);
-	}
-	else
-	{
-		shell_init(argv[0], fd);
+		shellname = argv[1];
 	}
 
+	/* Initialize shell */
+	shell_init(shellname, fd);
+
+	/* EXECUTION */
 	if (shell.interactive)
 		execute_hshrc();
-
 	execute_file(fd);
-	_getline(-1);
-	if (shell.interactive)
-		save_history_to_file();
-	for (i = 0; environ[i]; i++)
-		free(environ[i]);
-	free(environ);
-	while (shell.aliases)
-	{
-		tmp = shell.aliases;
-		shell.aliases = shell.aliases->next;
-		free(tmp->alias), free(tmp->value), free(tmp);
-	}
+
+	/* CLEAN UP */
+	shell_cleanup();
 	return (shell.status);
+
 }
 
 /**
@@ -60,10 +50,14 @@ void shell_init(char *shellname, int input)
 {
 	int i;
 
-	shell.name = shellname;
-	shell.interactive = isatty(input);
-	shell.status = 0;
+	shell.run = true;
 	shell.pid = getpid();
+	shell.name = shellname;
+	shell.lines = 1;
+	shell.status = 0;
+	shell.aliases = NULL;
+	shell.interactive = isatty(input);
+	environ = _realloc_string_array(environ, 0);
 	if (shell.interactive)
 	{
 		shell.history = malloc(sizeof(char *) * HISTSIZE);
@@ -71,8 +65,95 @@ void shell_init(char *shellname, int input)
 			shell.history[i] = NULL;
 		shell.history_size = get_history(shell.history);
 	}
-	shell.lines = 1;
-	shell.run = true;
-	shell.aliases = NULL;
-	environ = _realloc_string_array(environ, 0);
+}
+
+/**
+ * shell_cleanup - performs memory cleanups
+ **/
+void shell_cleanup(void)
+{
+	int i;
+	alias_t *tmp;
+
+	/* free line */
+	_getline(-1);
+
+	/* Save history to history file */
+	if (shell.interactive)
+		save_history_to_file();
+
+	/* free environ array */
+	for (i = 0; environ[i]; i++)
+		free(environ[i]);
+	free(environ);
+
+	/* free aliases */
+	while (shell.aliases)
+	{
+		tmp = shell.aliases;
+		shell.aliases = shell.aliases->next;
+		free(tmp->alias), free(tmp->value), free(tmp);
+	}
+}
+
+/**
+ * execute_file - executes a file line by line
+ * @fd: file descriptor
+ * Return: exit status
+ **/
+void execute_file(int fd)
+{
+	char *prompt, **tokens = NULL;
+
+	while (shell.run)
+	{
+		/* get and write shell prompt */
+		prompt = get_prompt(fd);
+		write(fd, prompt, _strlen(prompt));
+		signal(SIGINT, sigint_handler);
+
+		/* respond to user inputs */
+		tokens = get_tokens(fd);
+		if (tokens == NULL)
+		{
+			free(prompt);
+			break;
+		}
+
+		/* execute line */
+		execute_line(tokens);
+		shell.lines++;
+		free(tokens);
+
+		/* cleanup */
+		free(prompt);
+	}
+}
+
+/**
+ * execute_hshrc - executes the .hshrc file if it exists
+ * Return: exit status of execute_file()
+ **/
+void execute_hshrc(void)
+{
+	int hshrc_fd;
+	char *homedir, path[256], error_msg[256];
+
+	/* create path for hshrc */
+	homedir = _getenv("HOME");
+	sprintf(path, "%s/%s", homedir, ".hshrc");
+	free(homedir);
+
+	/* go get .hshrc file descriptor */
+	hshrc_fd = open(path, O_RDONLY);
+	if (hshrc_fd == -1)
+	{
+		if (errno != ENOENT)
+		{
+			sprintf(error_msg, "%s: 0: Can't open %s\n", shell.name, path);
+			write(STDERR_FILENO, error_msg, _strlen(error_msg));
+		}
+	}
+	else
+		execute_file(hshrc_fd);
 }
